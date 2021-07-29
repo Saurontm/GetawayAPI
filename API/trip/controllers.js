252889bcd9cf4f2ee31da2ac36f901acf757fe4e
Sarah Almahmid <hpsarontm@gmail.com>
2021-07-29 18:34:16 +0300

    add new trip as user

--- API/trip/controllers.js
+++ API/trip/controllers.js
@@ -1,4 +1,4 @@
-const { Trip } = require("../../db/models");
+const { Trip, User } = require("../../db/models");
 
 exports.fetchTrip = async (tripId, next) => {
   try {
@@ -13,9 +13,23 @@ exports.tripsFetch = async (req, res, next) => {
   try {
     const trips = await Trip.findAll({
       attributes: { exclude: ["createdAt", "updatedAt"] },
-      //   include: { model: User, as: "user", attributes: ["name"] },
+      include: { model: User, as: "user", attributes: ["username"] },
     });
+
     res.json(trips);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createTrip = async (req, res, next) => {
+  try {
+    req.body.userId = req.user.id;
+    if (req.file) req.body.image = `http://${req.get("host")}/${req.file.path}`;
+
+    const newTrip = await Trip.create(req.body);
+
+    res.status(201).json(newTrip);
   } catch (error) {
     next(error);
   }
--- API/trip/routes.js
+++ API/trip/routes.js
@@ -1,9 +1,24 @@
 const express = require("express");
-
-const { tripsFetch, fetchTrip } = require("./controllers");
+const passport = require("passport");
+const { tripsFetch, fetchTrip, createTrip } = require("./controllers");
 
 const router = express.Router();
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: "./media",
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
+const upload = multer({ storage });
 
 router.get("/", tripsFetch);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  upload.single("image"),
+  createTrip
+);
 
 module.exports = router;
