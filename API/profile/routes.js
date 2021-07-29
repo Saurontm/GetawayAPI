010dbea6faf67761a01d1b633651717a07681a35
Athari <athari@Atharis-MacBook-Pro.local>
2021-07-29 17:01:18 +0300

    sign in and up

---
+++ API/profile/routes.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const multer = require("multer");
+const passport = require("passport");
+
+const {
+    profileFetch,
+    fetchProfile,
+} = require("./controllers");
+const router = express.Router();
+
+//multer
+const storage = multer.diskStorage({
+    destination: "./media",
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}${file.originalname}`);
+    },
+});
+const upload = multer({ storage });
+
+// param middleware (parameter)
+router.param("profileId", async (req, res, next, profileId) => {
+    // get the profile with id profileId
+    const profile = await fetchProfile(profileId, next);
+    if (profile) {
+        // store it in req
+        req.profile = profile;
+        next();
+    } else {
+        // give back response 404 Profile Not Found
+        const error = new Error("Profile Not Found.");
+        error.status = 404;
+        next(error);
+    }
+});
+
+// List Route
+router.get("/", profileFetch);
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,22 +1,57 @@
 const express = require("express");
-const db = require("./db/models");
-const users = require("./users");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
+//routes
+const profileRoutes = require("./API/profile/routes");
+const userRoutes = require("./API/user/routes");
+
+const passport = require("passport");
+const { localStrategy } = require("./middleware/passport");
+const { jwtStrategy } = require("./middleware/passport");
+
+//DATEBASE
+const db = require("./db/models/index");
 const app = express();
 
-app.get("/users", (req, res) => {
-  res.json(users);
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(passport.initialize());
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+//=============== Getaway Routes ===============\\
+app.use("/profiles", profileRoutes);
+app.use(userRoutes);
+app.use("/media", express.static("media"));
+
+//========== Error Handling Middleware ==========\\
+app.use((err, req, res, next) => {
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || "internal Server Error." });
 });
 
+//=========Path Not Found===========\\
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Path not found." });
+});
+
+
+//app.get("/users", (req, res) => {
+//  res.json(users);
+//});
+//===============================\\
 const run = async () => {
   try {
-    await db.sequelize.authenticate();
+    await db.sequelize.sync({ alter: true });
     console.log("Connection to the database successful!");
-    await app.listen(8000, () => {
+    app.listen(8000, () => {
       console.log("The application is running on localhost:8000");
     });
   } catch (error) {
-    console.error("Error connecting to the database: ", error);
+    console.error(error);
   }
 };
 
